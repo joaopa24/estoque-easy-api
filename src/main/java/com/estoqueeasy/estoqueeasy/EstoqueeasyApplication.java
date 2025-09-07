b3e55da840dad0223a41b1a38219f68d04bf2f68
joaopa24 <joaokn129@gmail.com>
2025-09-07 17:37:43 -0300

    first commit

---
+++ src/main/java/com/estoqueeasy/estoqueeasy/EstoqueeasyApplication.java
@@ -0,0 +1,13 @@
+package com.estoqueeasy.estoqueeasy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstoqueeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstoqueeasyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estoqueeasy/estoqueeasy/EstoqueeasyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estoqueeasy.estoqueeasy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstoqueeasyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
